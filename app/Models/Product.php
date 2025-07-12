36b3a3f3431f05939ad61a75b1bb14979972cc9e
Hiroto N <hiroto4455656@gmail.com>
2025-07-13 07:33:59 +0900

    migration and define all the model classess

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,40 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
-{
-    //
+class Product extends Model {
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'brand_id',
+        'name',
+        'slug',
+        'description',
+        'images',
+        'price',
+        'is_active',
+        'is_featured',
+        'in_stock',
+        'on_sale'
+    ];
+
+    protected $casts = [
+        'images' => 'array'
+    ];
+
+    public function catetory() {
+        return $this->belongsTo( Category::class );
+    }
+
+    public function brand() {
+        return $this->belongsTo( Brand::class );
+    }
+
+    public function orderItems() {
+        return $this->hasMany( OrderItem::class );
+    }
+
 }
