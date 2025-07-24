f5b3f1b917a55bc5950464e55d637da7ebc885fd
Hiroto N <hiroto4455656@gmail.com>
2025-07-25 07:09:17 +0900

    all the models defined

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -5,7 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Address extends Model
-{
+class Address extends Model {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function order() {
+        return $this->belongsTo( Order::class );
+    }
+
+    public function getFullNameAttribute() {
+        return "{$this->first_name} {$this->last_name}";
+    }
 }
