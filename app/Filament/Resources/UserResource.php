af6de187d6b8431dbd10caf4a070328e041bae89
Hiroto N <hiroto4455656@gmail.com>
2025-07-26 05:33:55 +0900

    add view and edit actions to the User Page

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -7,11 +7,14 @@ use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Pages\Page;
+use Filament\Resources\Pages\CreateRecord;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
 
 class UserResource extends Resource {
     protected static ?string $model = User::class;
@@ -25,13 +28,19 @@ class UserResource extends Resource {
             ->required()
             ->maxLength( 255 ),
             Forms\Components\TextInput::make( 'email' )
+            ->label( 'Email Address' )
             ->email()
-            ->required()
-            ->maxLength( 255 ),
-            Forms\Components\DateTimePicker::make( 'email_verified_at' ),
+            ->maxLength( 255 )
+            ->unique( ignoreRecord: true )
+            ->required(),
+            Forms\Components\DateTimePicker::make( 'email_verified_at' )
+            ->label( 'Email Verified At' )
+            ->default( now() ),
             Forms\Components\TextInput::make( 'password' )
             ->password()
-            ->required()
+            ->dehydrateStateUsing( fn( $state ) => Hash::make( $state ) )
+            ->dehydrated( fn( $state ) => filled( $state ) )
+            ->required( fn( Page $livewire ): bool => $livewire instanceof CreateRecord )
             ->maxLength( 255 ),
         ] );
     }
@@ -59,7 +68,10 @@ class UserResource extends Resource {
             //
         ] )
         ->actions( [
+            Tables\Actions\ActionGroup::make
+            ( [ Tables\Actions\ViewAction::make(),
             Tables\Actions\EditAction::make(),
+            Tables\Actions\DeleteAction::make(), ] )
         ] )
         ->bulkActions( [
             Tables\Actions\BulkActionGroup::make( [
