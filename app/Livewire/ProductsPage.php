93b9486e2b8870f1bcf2679d2b83164819837750
Hiroto N <hiroto4455656@gmail.com>
2025-09-19 08:11:48 +0900

    cart adding functions

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
 use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
@@ -33,6 +35,14 @@ class ProductsPage extends Component {
 
     #[ Url ]
     public $sort = 'latest';
+
+    // add product to cart method
+
+    public function addToCart( $product_id ) {
+        $total_count = CartManagement::addItemToCart( $product_id );
+
+        $this->dispatch( 'update-cart-count', total_count: $total_count )->to( Navbar::class );
+    }
 
     public function render() {
         $productQuery = Product::query()->where( 'is_active', 1 );
