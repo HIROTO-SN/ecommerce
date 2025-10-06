f7e558ea8da2c232a390d795e6cf19ea5cffe8d7
Hiroto N <hiroto4455656@gmail.com>
2025-10-07 06:26:15 +0900

    stripe payment implemented

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -3,8 +3,12 @@
 namespace App\Livewire;
 
 use App\Helpers\CartManagement;
+use App\Models\Address;
+use App\Models\Order;
 use Livewire\Attributes\Title;
 use Livewire\Component;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
 
 #[ Title( 'Checkout' ) ]
 
@@ -20,6 +24,7 @@ class CheckoutPage extends Component {
     public $payment_method;
 
     public function placeOrder() {
+
         $this->validate( [
             'first_name'      => 'required',
             'last_name'       => 'required',
@@ -30,6 +35,78 @@ class CheckoutPage extends Component {
             'zip_code'        => 'required',
             'payment_method'  => 'required',
         ] );
+
+        $cart_items = CartManagement::getCartItemsFromCookie();
+
+        $line_items = [];
+
+        foreach ( $cart_items as $item ) {
+            $line_items[] = [
+                'price_data' => [
+                    'currency' => 'USD',
+                    'unit_amount' => $item[ 'unit_amount' ] * 100,
+                    'product_data' => [
+                        'name' => $item[ 'name' ],
+                    ],
+                ],
+                'quantity' => $item[ 'quantity' ],
+            ];
+        }
+
+        $order = new Order();
+        $order->user_id = auth()->user()->id;
+        $order->grand_total = CartManagement::calculateGrandTotal( $cart_items );
+        $order->payment_method = $this->payment_method;
+        $order->payment_status = 'pending';
+        $order->status = 'new';
+        $order->currency = 'USD';
+        $order->shipping_amount = 0;
+        $order->shipping_method = 'none';
+        $order->notes = 'Order placed by '. auth()->user()->name;
+
+        $address = new Address();
+        $address->first_name = $this->first_name;
+        $address->last_name = $this->last_name;
+        $address->phone = $this->phone;
+        $address->street_address = $this->street_address;
+        $address->city = $this->city;
+        $address->state = $this->state;
+        $address->zip_code = $this->zip_code;
+
+        $redirect_url = '';
+
+        if ( $this->payment_method == 'stripe' ) {
+            Stripe::setApiKey( env( 'STRIPE_SECRET' ) );
+            $sessionCheckout = Session::create( [
+                'payment_method_types' => [ 'card' ],
+                'customer_email' => auth()->user()->email,
+                'line_items' => $line_items,
+                'mode' => 'payment',
+                'success_url' => route( 'success' ).'?session_id={CHECKOUT_SESSION_ID}',
+                'cancel_url' => route( 'cancel' ),
+            ] );
+
+            $redirect_url = $sessionCheckout->url;
+        } else {
+            $redirect_url = route( 'success' );
+        }
+
+        $order->save();
+        $address->order_id = $order->id;
+        $address->save();
+        $order->items()->createMany(
+            collect($cart_items)->map(function ($item) {
+                return [
+                    'product_id'   => $item['product_id'],
+                    'quantity'     => $item['quantity'],
+                    'unit_amount'  => $item['unit_amount'],
+                    'total_amount' => $item['total_amount'],
+                ];
+            })->toArray()
+        );
+        CartManagement::cleanCartItems();
+
+        return redirect( $redirect_url );
     }
 
     public function render() {
