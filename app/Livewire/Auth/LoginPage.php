83976a11de155060e02200fad23f03865c066bf0
Hiroto N <hiroto4455656@gmail.com>
2025-11-17 17:17:22 +0900

    fortify installed

--- app/Livewire/Auth/LoginPage.php
+++ app/Livewire/Auth/LoginPage.php
@@ -2,30 +2,47 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
 use Livewire\Attributes\Title;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 #[ Title( 'Login' ) ]
 
 class LoginPage extends Component {
-
     public $email;
     public $password;
+
+    protected $casts = [
+        'two_factor_secret' => 'encrypted', // 自動暗号化
+        '   ' => 'encrypted',
+    ];
 
     public function render() {
         return view( 'livewire.auth.login-page' );
     }
 
     public function save() {
+        // バリデーション
         $this->validate( [
-            'email' => 'required|email|exists:users,email|max:255',
+            'email' => 'required|email|max:255',
             'password' => 'required|min:6|max:255',
         ] );
 
-        if ( !auth()->attempt( [ 'email'=>$this->email, 'password'=>$this->password ] ) ) {
+        $user = User::where( 'email', $this->email )->first();
+        if ( !$user || !Hash::check( $this->password, $user->password ) ) {
             session()->flash( 'error', 'Invalid credentials' );
             return;
         }
-        return redirect()->intended();
+
+        if ( $user->two_factor_secret ) {
+            session( [ 'login.id' => $user->id ] );
+            return redirect()->route( 'two-factor.login' );
+        }
+        Auth::login( $user );
+        session()->regenerate();
+        return redirect()->intended( '/' );
     }
 }
